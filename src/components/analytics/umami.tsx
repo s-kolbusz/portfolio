69414fd6d506426668e544f18f0791f3ee1a18ea
Sebastian Kolbusz <s.kolbusz@outlook.com>
2026-04-09 11:53:26 +0200

    feat(umami): Add performance tracking to umami analytics

--- src/components/analytics/umami.tsx
+++ src/components/analytics/umami.tsx
@@ -5,5 +5,13 @@ export const UmamiAnalytics = () => {
   const websiteId = process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID
   if (!websiteId || !websiteURL) return null
 
-  return <Script async src={websiteURL} data-website-id={websiteId} strategy="afterInteractive" />
+  return (
+    <Script
+      async
+      src={websiteURL}
+      data-website-id={websiteId}
+      strategy="afterInteractive"
+      data-performance="true"
+    />
+  )
 }
